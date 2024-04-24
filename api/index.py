384dcf4a2f0517d534c9ffd5c307a8da9d2656fe
lukewealth <lukemonry@gmail.com>
2024-04-24 14:12:07 +0100

    modifying flask app

--- api/index.py
+++ api/index.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from flask import Flask,request
+from main import chat_with_sui
+
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods =["POST"])
 def home():
-    return 'Hello, World!'
+    if request.is_json:
+        data = request.get_json()
+        message = data.get('message')
+        return f'{message}'
+    else:
+     return 'Invalid request format. Please send JSON data.'
+    # return 'Hello, World!'
 
 @app.route('/about')
 def about():
