8ad20d0cd6a84b4ef9489c02430e1d900d020dc9
lukewealth <lukemonry@gmail.com>
2024-05-07 18:02:42 +0100

    improved web scrapping to automatically add json string

--- codes.py
+++ codes.py
@@ -10,7 +10,7 @@ username = os.environ.get('USER')
 
 
 # url = "https://api.github.com/repos/overmind-xyz/jpeg-jackpot-clmdpmkw20000jt083z0y186x/blob/main/sources/nft_lottery.move"
-url = "https://github.com/Ovodo/pool/blob/main/sources/modules.md"
+url = "https://move-book.com/move-basics/generics.html"
 page = requests.get(url)
 
 
@@ -19,7 +19,7 @@ def format_article(article_html):
     soup = BeautifulSoup(article_html, 'html.parser')
     
     # Find the <article> tag
-    article_tag = soup.find("article")
+    article_tag = soup.find("main")
     
     if article_tag:
               # Find all <h1>, <h2>, <h3>, <h4>, <h5>, <h6> tags within the <article> tag and add '#' symbols based on their level
@@ -27,11 +27,15 @@ def format_article(article_html):
             header_level = header.name[1]  # Extract the header level from the tag name
             header.string = f"{'#' * int(header_level)} {header.text}\n\n"
         
+        # Find all <code> tags within the <article> tag and wrap them in triple backticks
+        for highlight_tag in article_tag.find_all('code'):
+            highlight_tag.string = f"`{highlight_tag.text}`\n"
+            
         # Find all <p> tags within the <article> 
-        # for p_tag in article_tag.find_all('p'):
-        #     p_tag.string = f"{p_tag.text}\n"
+        for p_tag in article_tag.find_all('p'):
+            p_tag.string = f"{p_tag.text}\n"
         
-                # Find all <a> tags within the <article> tag and format them as Markdown links
+        # Find all <a> tags within the <article> tag and format them as Markdown links
         for a_tag in article_tag.find_all('a'):
             href = a_tag.get('href')
             a_tag.string = f"[{a_tag.text}]({href})"
@@ -56,14 +60,26 @@ def format_article(article_html):
             ol_tag.replace_with(''.join([f"{counter}. {item.text}\n" for counter, item in enumerate(list_items, 1)]))
 
         
-        # Find all <code> tags within the <article> tag and wrap them in triple backticks
-        for highlight_tag in article_tag.find_all('code'):
-            highlight_tag.string = f"`{highlight_tag.text}`\n"
-            
             
         # Find all <code> tags within the <article> tag and wrap them in triple backticks
         for code_tag in article_tag.find_all('pre'):
             code_tag.string = f"```move{code_tag.text}```\n"
+            
+           # Find all <table> tags within the <article> tag and format them as Markdown tables
+        for table_tag in article_tag.find_all('table'):
+            table_string = "|"
+            for th in table_tag.find_all('th'):
+                table_string += f" {th.text} |"
+            table_string += "\n|"
+            for _ in range(len(table_string) - 1):
+                table_string += "-"
+            table_string += "|\n"
+            for tr in table_tag.find_all('tr')[1:]:
+                for td in tr.find_all('td'):
+                    table_string += f" {td.text} |"
+                table_string += "\n"
+            table_string += "\n"
+            table_tag.replace_with(table_string)
         
         # Extract text content from the <article> tag and return it without extra spaces
         formatted_article = ''.join([str(tag.text) for tag in article_tag.contents])
@@ -97,10 +113,13 @@ article_html = """
 
 formatted_article = format_article(page.text)
 # Convert the dictionary to a JSON string with pretty formatting
-json_string = json.dumps(formatted_article, indent=4, ensure_ascii=False)
-content_dict = {"messages":[{"role": "system", "content": "SuiAI is a chat agent that’s proficient in the Move smart contract language"}, {"role": "user", "content": "Using the sui move programming language, create a simple dice game where two players roll a dice for three rounds, and at the end, the player with the highest score wins. if it is a tie, then they go again."},{"role":"assistant","content": formatted_article}]}
+content_dict = {"messages":[{"role": "system", "content": "SuiAI is a chat agent that’s proficient in the Move smart contract language"}, {"role": "user", "content": "What are `Generics` in the sui move language?"},{"role":"assistant","content": formatted_article}]}
+json_string = json.dumps(content_dict,ensure_ascii=False)
 
-print(content_dict)
+# Create or open the .jsonl file
+with open('training/move_basics.jsonl', 'a', encoding='utf-8') as file:
+    # Write the JSON string to the file
+    file.write(json_string + '\n')
 
 
 
